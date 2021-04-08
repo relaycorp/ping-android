9e1174b985c2b164850530d2c1fc612c9b0fb5aa
Sérgio Santos <me@sergiosantos.info>
2021-04-08 16:57:47 +0100

    feat: Handle no gateway

--- app/src/main/java/tech/relaycorp/ping/App.kt
+++ app/src/main/java/tech/relaycorp/ping/App.kt
@@ -1,17 +1,22 @@
 package tech.relaycorp.ping
 
 import android.app.Application
+import android.content.Intent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import tech.relaycorp.awaladroid.Awala
+import tech.relaycorp.awaladroid.GatewayBindingException
 import tech.relaycorp.awaladroid.GatewayClient
+import tech.relaycorp.ping.common.Logging.logger
 import tech.relaycorp.ping.common.di.AppComponent
 import tech.relaycorp.ping.common.di.DaggerAppComponent
 import tech.relaycorp.ping.domain.BootstrapData
 import tech.relaycorp.ping.domain.ReceivePong
+import tech.relaycorp.ping.ui.main.NoGatewayActivity
+import java.util.logging.Level
 import javax.inject.Inject
 
 open class App : Application() {
@@ -40,8 +45,20 @@ open class App : Application() {
 
     protected open suspend fun setupAwala() {
         Awala.setup(this)
-        GatewayClient.bind()
-        bootstrapData.bootstrapIfNeeded()
-        GatewayClient.receiveMessages().collect(receivePong::receive)
+        try {
+            GatewayClient.bind()
+            bootstrapData.bootstrapIfNeeded()
+            GatewayClient.receiveMessages().collect(receivePong::receive)
+        } catch (exp: GatewayBindingException) {
+            logger.log(Level.WARNING, "Gateway binding exception", exp)
+            openNoGateway()
+        }
+    }
+
+    private fun openNoGateway() {
+        startActivity(
+            NoGatewayActivity.getIntent(this)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        )
     }
 }
