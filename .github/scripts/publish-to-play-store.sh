482276f4b8da65fb425302d60353f134491b1365
Gus Narea <gnarea@users.noreply.github.com>
2021-04-16 19:09:37 +0100

    chore(release): Add debugging to identify issue with release

--- .github/scripts/publish-to-play-store.sh
+++ .github/scripts/publish-to-play-store.sh
@@ -7,6 +7,24 @@ set -o pipefail
 
 VERSION_NAME="$1"
 
+# TODO: Remove this debugging nonsense
+if [[ -z "${ANDROID_KEYSTORE}" ]]; then
+  echo "ANDROID_KEYSTORE is unset"
+else
+  echo "ANDROID_KEYSTORE is unset"
+fi
+if [[ -z "${ANDROID_KEYSTORE_PASSWORD}" ]]; then
+  echo "ANDROID_KEYSTORE_PASSWORD is unset"
+else
+  echo "ANDROID_KEYSTORE_PASSWORD is unset"
+fi
+echo "ANDROID_KEYSTORE_KEY_ALIAS = ${ANDROID_KEYSTORE_KEY_ALIAS}"
+if [[ -z "${ANDROID_KEYSTORE_KEY_PASSWORD}" ]]; then
+  echo "ANDROID_KEYSTORE_KEY_PASSWORD is unset"
+else
+  echo "ANDROID_KEYSTORE_KEY_PASSWORD is unset"
+fi
+
 set +x  # Disable debug (if enabled) so we won't leak secrets accidentally
 echo "${ANDROID_KEYSTORE}" | base64 -d > /tmp/keystore.jks
 exec ./gradlew app:publishApps \
